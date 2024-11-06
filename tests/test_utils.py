9a3958f7a5237445e5e30c6738ef6ef1f4c34441
Julien <julien.laffargue@gmail.com>
2024-11-06 21:43:33 +0100

    tests now with pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,37 +1,33 @@
-import unittest
+import pytest
 from utils import get_fuels, get_load, get_powerplants
 
-class TestUtils(unittest.TestCase):
+@pytest.fixture
+def payload():
+    return {
+        "fuels": {
+            "gas(euro/MWh)": 13.4,
+            "kerosine(euro/MWh)": 50.8,
+            "wind(%)": 60
+        },
+        "load": 480,
+        "powerplants": [
+            {"name": "gasfiredbig1", "type": "gasfired", "efficiency": 0.53, "pmin": 100, "pmax": 460},
+            {"name": "tj1", "type": "turbojet", "efficiency": 0.3, "pmin": 0, "pmax": 16},
+            {"name": "windpark1", "type": "windturbine", "efficiency": 1, "pmin": 0, "pmax": 150}
+        ]
+    }
 
-    def setUp(self):
-        self.payload = {
-            "fuels": {
-                "gas(euro/MWh)": 13.4,
-                "kerosine(euro/MWh)": 50.8,
-                "wind(%)": 60
-            },
-            "load": 480,
-            "powerplants": [
-                {"name": "gasfiredbig1", "type": "gasfired", "efficiency": 0.53, "pmin": 100, "pmax": 460},
-                {"name": "tj1", "type": "turbojet", "efficiency": 0.3, "pmin": 0, "pmax": 16},
-                {"name": "windpark1", "type": "windturbine", "efficiency": 1, "pmin": 0, "pmax": 150}
-            ]
-        }
+def test_get_fuels(payload):
+    gas, kerosine, wind = get_fuels(payload)
+    assert gas == 13.4
+    assert kerosine == 50.8
+    assert wind == 60
 
-    def test_get_fuels(self):
-        gas, kerosine, wind = get_fuels(self.payload)
-        self.assertEqual(gas, 13.4)
-        self.assertEqual(kerosine, 50.8)
-        self.assertEqual(wind, 60)
+def test_get_load(payload):
+    load = get_load(payload)
+    assert load == 480
 
-    def test_get_load(self):
-        load = get_load(self.payload)
-        self.assertEqual(load, 480)
-
-    def test_get_powerplants(self):
-        powerplants = get_powerplants(self.payload)
-        self.assertEqual(len(powerplants), 3)
-        self.assertEqual(powerplants[0]["name"], "gasfiredbig1")
-
-if __name__ == "__main__":
-    unittest.main()
+def test_get_powerplants(payload):
+    powerplants = get_powerplants(payload)
+    assert len(powerplants) == 3
+    assert powerplants[0]["name"] == "gasfiredbig1"
